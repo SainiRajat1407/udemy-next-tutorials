b7e7b696b94564f64bb3b71683c4991f1e2891ce
Rajat Rajat <rajrajat@sheridancollege.ca>
2024-07-03 16:09:05 -0400

    Using client Components Efficiently

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -1,30 +1,35 @@
-import logoImage from '@/assets/logo.png'
-import Link from 'next/link'
-import classes from './main-header.module.css'
-import Image from 'next/image'
-import MainHeaderBackground from './main-header-background'
 
-export default function MainHeader(){
-    return(
-       <>
-       <MainHeaderBackground />
-        <header className={classes.header}>
-            <Link className={classes.logo} href="/">
-                <Image src={logoImage} priority={true} alt="A plate with food on it." />
-                NextLevel Food
-            </Link>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Foodie Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        
-       </>
-    )
+import logoImage from "@/assets/logo.png";
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImage}
+            priority={true}
+            alt="A plate with food on it."
+          />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals" >Browse Meals</NavLink>
+            </li>
+            <li>
+            <NavLink href="/community" >Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
