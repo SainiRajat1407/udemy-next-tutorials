240606f7efcd24d36ae6187d8d927e28b31fe149
rajat rajat <rajrajat@sheridancollege.ca>
2024-07-07 15:21:26 -0400

    adding server action to the form submission on the share meal page

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,19 +1,37 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
-    const imageInputRef = useRef();
+  const imageInputRef = useRef();
+  const [pickedImage, setPickedImage] = useState();
 
-    function handlePickClick(){
-        imageInputRef.current.click();
+  function handlePickClick() {
+    imageInputRef.current.click();
+  }
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+        setPickedImage(null);
+      return;
     }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
   return (
     <>
       <div className={classes.picker}>
         <label htmlFor="image">{label}</label>
         <div className={classes.controls}>
+          <div className={classes.preview}>
+            {pickedImage && <Image src={pickedImage} alt="Picked" fill />}
+            {!pickedImage && <p>No image picked yet.</p>}
+          </div>
           <input
             className={classes.input}
             type="file"
@@ -21,8 +39,14 @@ export default function ImagePicker({ label, name }) {
             accept="image/png, image/jpeg"
             name={name}
             ref={imageInputRef}
+            onChange={handleImageChange}
+            required
           />
-          <button className={classes.button} type="button" onClick={handlePickClick}>
+          <button
+            className={classes.button}
+            type="button"
+            onClick={handlePickClick}
+          >
             Pick an Image
           </button>
         </div>
