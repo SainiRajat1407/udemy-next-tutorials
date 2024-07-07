ddf8881c4972749fab30979e4907d1c2724ad484
rajat rajat <rajrajat@sheridancollege.ca>
2024-07-07 15:02:08 -0400

    setting up image picker

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
-import ImageSlideshow from "@/components/images/image-slides";
+import ImageSlideshow from "../components/images/image-slides";
 
 
 export default function Home() {
