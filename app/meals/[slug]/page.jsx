0cf743a329e5033d570aad859fbd9fb78f484907
rajat rajat <rajrajat@sheridancollege.ca>
2024-07-07 16:42:00 -0400

    foodie app build

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -3,6 +3,20 @@ import classes from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata(){
+  const meal = getMeal(params.mealSlug)
+
+  if (!meal) {
+    notFound();
+  }
+
+    return {
+        title: meal.title,
+        description: meal.summary,
+    };
+
+}
+
 export default function MealsDetailsPage({params}) {
 
     const meal = getMeal(params.slug);
--- components/meals/meals-grid.jsx
+++ components/meals/meals-grid.jsx
@@ -6,8 +6,9 @@ import classes from './meals-grid.module.css'
 export default function MealsGrid({meals}){
     return (
         <ul className={classes.meals}>
-            {meals.map((meal) => (
+            {meals.map((meal, index) => (
                 <MealItem
+                key={index}
                    {...meal}
                 />
             ))}
