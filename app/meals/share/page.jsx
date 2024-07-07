71a3ddf639d8fe252c5a8b87c8db9cf7df68f244
rajat rajat <rajrajat@sheridancollege.ca>
2024-07-07 15:50:09 -0400

    Share meal form working

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,20 +1,11 @@
+
 import classes from "./page.module.css";
 import ImagePicker from "../../../components/meals/image-picker"
+import {shareMeal} from '@/lib/action'
 
 export default function ShareMealPage() {
 
-  async function shareMeal(formData){
-    'use server' // this is the server action function should be a async function
-    const meal = {
-      title : formData.get('title'),
-      summary : formData.get('summary'),
-      instructions : formData.get('instructions'),
-      image : formData.get('image'),
-      creator : formData.get('name'),
-      creator_email : formData.get('email')
-    }
-    console.log(meal)
-  }
+  
   return (
     <>
       <header className={classes.header}>
