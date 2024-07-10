2c3cffc054c2aca7c279fe4454ae3efb0c465eb9
Rajat Rajat <rajrajat@sheridancollege.ca>
2024-07-10 13:25:10 -0400

    option2 for handling server functions

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,12 +1,22 @@
-import Link from "next/link";
-import { DUMMY_NEWS } from "@/dummy-news";
+
 import NewsList from "@/components/news-llst";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+    const response = await fetch("http://localhost:8080/news");
+    const news = await response.json();
+  if(!response.ok){
+    throw new Error("Something went wrong while fetching news")
+  }
+
+  let newsContent ;
+
+  if(news){
+    newsContent = <NewsList news={news}/>
+  }
   return (
     <div id="news">
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS}/>
+      {newsContent}
     </div>
   );
 }
